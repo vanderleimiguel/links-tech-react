6c5e5471f9e6b17d37fcc0c735901ac3563fd7a8
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-09-18 23:41:46 -0300

    create footer

---
+++ vite-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+export function Footer() {
+  return (
+    <div className="footer">
+      <a target="_blank" href="https://vanderleimiguel.herokuapp.com/">
+        <p>
+          &copy; Copyright <strong>Vanderlei Miguel</strong>
+        </p>
+      </a>
+    </div>
+  );
+}
