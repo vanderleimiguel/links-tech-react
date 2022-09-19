71731fee7b47bb228f75c4c3fed0ae0d9068fe9d
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-09-19 19:37:28 -0300

    update css and edit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import { Home } from './pages/Home/Home'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Summary } from './components/Summary/Summary'
+// import { Webcrawler } from './components/Webcrawler/Webcrawler'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <Summary />
       <Footer />
+      {/* <Webcrawler /> */}
     </div>
   )
 }
